75a71d58c81a97c0a6d792e58a990506bb4e5352
istekhar <istekhararkam25@gmail.com>
2024-11-25 20:37:11 +0530

    done

--- src/Components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import UserContext from "../context/UserContext";
-const Profile = () => {
-  const { user } = useContext(UserContext);
-  if (!user) return <div>please login</div>;
-  return <div>welcome{user.userName}</div>;
-};
-
-export default Profile;
